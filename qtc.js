928f478f4fc35007f08c37c7607bc26eb7195411
50m30n3 <someone@d00m.org>
2012-02-23 16:59:24 +0100

    Added license and readme

--- qtc.js
+++ qtc.js
@@ -1,3 +1,22 @@
+/*
+*    QTW: qtc.js (c) 2011, 2012 50m30n3
+*
+*    This file is part of QTW.
+*
+*    QTW is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    QTW is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with QTW.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 "use strict";
 
 function qtcDecode( image, refimage, minx, maxx, commanddata, imagedata, width, height, minsize, maxdepth )
